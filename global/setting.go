1cae05ea1d2d16f4423d7c78e6a95768f4438698
jhq <2098128799@qq.com>
2024-09-14 23:46:17 +0800

    new

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "blog-service/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettings
+	AppSetting      *setting.AppSettings
+	DatabaseSetting *setting.DatabaseSettings
+)
--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	v1 "blog-service/internal/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,19 +9,22 @@ func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	// 实例化
+	article := new(v1.Article)
+	tag := new(v1.Tag)
 
 	apiv1 := r.Group("/api/v1")
 	{
-		apiv1.POST("/tags")
-		apiv1.DELETE("/tags/:id")
-		apiv1.PUT("/tags/:id")
-		apiv1.GET("/tags")
+		apiv1.POST("/tags", tag.Create)
+		apiv1.DELETE("/tags/:id", tag.Delete)
+		apiv1.PUT("/tags/:id", tag.Update)
+		apiv1.GET("/tags", tag.List)
 
-		apiv1.POST("/articles")
-		apiv1.DELETE("/articles/:id")
-		apiv1.PUT("/articles/:id")
-		apiv1.GET("/articles/:id")
-		apiv1.GET("/articles")
+		apiv1.POST("/articles", article.Create)
+		apiv1.DELETE("/articles/:id", article.Delete)
+		apiv1.PUT("/articles/:id", article.Update)
+		apiv1.GET("/articles/:id", article.Get)
+		apiv1.GET("/articles", article.List)
 	}
 
 	return r
